d79ba4a1eee3f0ee1b1c842f2e46852d46cd2804
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-18 23:43:30 +0530

    done

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var mmysqlConnection = mysql.createConnection({
     host: 'localhost',
     user : 'root',
     password : '',
-    database : 'employeedb'
+    database : 'employeedb',
+    multipleStatements: true
 });
 
 mmysqlConnection.connect((err)=>{
@@ -22,10 +23,43 @@ mmysqlConnection.connect((err)=>{
 app.listen(3000,()=>console.log('Express sercer is running at port no : 3000'));
 
 //Get all employees
-app.get('/employee',(res,req)=>{
+app.get('/employee',(req,res)=>{
     mmysqlConnection.query('SELECT * FROM employee',(err, rows, fields)=>{
         if(!err)
-        console.log(rows[0].EmpID);
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+//Get an employee
+app.get('/employee/:id',(req,res)=>{
+    mmysqlConnection.query('SELECT * FROM employee WHERE EmpID = ?',[req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+//Delete an employee
+app.delete('/employee/:id',(req,res)=>{
+    mmysqlConnection.query('DELETE FROM employee WHERE EmpID = ?',[req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Deleted successfully.');
+        else
+        console.log(err);
+    })
+});
+
+//Insert an employee
+app.post('/employee',(req,res)=>{
+    let emp = req.body;
+    var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?;\
+    CALL employeeAddorEdit(@EmpID,@Name,@EmpCode,@Salary);";
+    mmysqlConnection.query(sql , [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],(err, rows, fields)=>{
+        if(!err)
+        res.send('Updated successfully');
         else
         console.log(err);
     })
